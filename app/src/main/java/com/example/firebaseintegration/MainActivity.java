3022b12432bf7a85a5517b85dc269b0689995cce
Harnoor <hsingh9@student.gsu.edu>
2019-04-14 15:25:15 -0400

    image push works

--- app/src/main/java/com/example/firebaseintegration/MainActivity.java
+++ app/src/main/java/com/example/firebaseintegration/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.firebaseintegration;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -16,19 +26,29 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements MyRecyclerViewAdapter.ItemClickListener {
 
     private DatabaseReference myRef;
+    private static final int MULTIPLE_PERMISSIONS = 10; // code you want.
 
-    private EditText etFirstName;
-    private EditText etLastName;
+
+    String currentPhotoPath;
     private Button btnUploadData;
+    private Button btnCamera;
     private List<Record> recordList;
     private RecyclerView recyclerView;
     MyRecyclerViewAdapter adapter;
+
+    private ImageView imgView;
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
 
     @Override
@@ -36,15 +56,21 @@ public class MainActivity extends AppCompatActivity implements MyRecyclerViewAda
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (checkPermissions()) {
+            //  permissions  granted.
+        }
+
         recordList = new ArrayList<>();
         recyclerView = findViewById(R.id.rvRecords);
         adapter = new MyRecyclerViewAdapter(getApplicationContext(), recordList);
         recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
         recyclerView.setAdapter(adapter);
 
-        etFirstName = findViewById(R.id.etFirstName);
-        etLastName = findViewById(R.id.etLastName);
+//        etFirstName = findViewById(R.id.etFirstName);
+//        etLastName = findViewById(R.id.etLastName);
         btnUploadData = findViewById(R.id.btnUploadData);
+        btnCamera = findViewById(R.id.btnCamera);
+        imgView = findViewById(R.id.imgView);
 
         myRef = FirebaseDatabase.getInstance().getReference("names");
 
@@ -75,19 +101,113 @@ public class MainActivity extends AppCompatActivity implements MyRecyclerViewAda
         btnUploadData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btnUploadDataAction(etFirstName.getText().toString(), etLastName.getText().toString());
+//                btnUploadDataAction(etFirstName.getText().toString(), etLastName.getText().toString());
+            }
+        });
+
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Take Picture Tapped", Toast.LENGTH_SHORT).show();
+                dispatchTakePictureIntent();
             }
         });
     }
 
-    private void btnUploadDataAction(String a, String b) {
-        String id = myRef.push().getKey();
-        Record record = new Record(id, a, b);
-        myRef.child(id).setValue(record);
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.firebaseintegration.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE); // changed to image capture
+
+            }
+        }
     }
+
+
+    /**
+     * returns unique file name and sotre it
+     *
+     * @return fileName
+     * @throws IOException
+     */
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
 
     @Override
     public void onItemClick(View view, int position) {
         Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+
+            galleryAddPic();
+            //            Bundle extras = data.getExtras();
+//            Bitmap imageBitmap = (Bitmap) extras.get("data");
+//            imgView.setImageBitmap(imageBitmap);
+        }
+    }
+
+
+    String[] permissions = new String[]{
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA,
+            Manifest.permission.ACCESS_FINE_LOCATION};
+
+
+    private boolean checkPermissions() {
+        int result;
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        for (String p : permissions) {
+            result = ContextCompat.checkSelfPermission(MainActivity.this, p);
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                listPermissionsNeeded.add(p);
+            }
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), MULTIPLE_PERMISSIONS);
+            return false;
+        }
+        return true;
     }
 }
