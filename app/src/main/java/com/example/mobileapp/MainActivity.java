1fcbd08caf77f8e039d0ea468ec96ba8414e84ab
arilok <44273343+arilok@users.noreply.github.com>
2019-04-23 11:51:08 -0400

    Add files via upload

--- app/src/main/java/com/example/mobileapp/MainActivity.java
+++ app/src/main/java/com/example/mobileapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.mobileapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToLogin(View view){
+        Intent mainIntent = new Intent(this, LoginActivity.class);
+        startActivity(mainIntent);
     }
 }
