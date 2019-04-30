c428072ce37ecdf34f78080d7078ec1e9978d595
tomato516 <jchoi1516@gmail.com>
2019-04-29 22:44:01 -0400

    Added key

--- app/src/main/java/com/example/mobileapp/ProfileActivity.java
+++ app/src/main/java/com/example/mobileapp/ProfileActivity.java
@@ -3,6 +3,7 @@ package com.example.mobileapp;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
@@ -11,6 +12,14 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.mobileapp.camera.ImagesActivity;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class ProfileActivity extends AppCompatActivity {
 
     private TextView clientName;
@@ -18,15 +27,17 @@ public class ProfileActivity extends AppCompatActivity {
     private TextView clientNum;
     private Button stylesButton;
     private ImageView imgView;
+    String key;
+
+    FirebaseDatabase db = FirebaseDatabase.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        //fetch customer data from DB and set to object below
-
-        Client c = new Client("Jenny", "Choi", "loves black coffee", "404-435-2051", "dkfjdkf");
+        if (getIntent().hasExtra("keyValue")) {
+            key =  getIntent().getStringExtra("keyValue");}
 
         //bind data to views
         clientName = findViewById(R.id.profileName);
@@ -35,14 +46,39 @@ public class ProfileActivity extends AppCompatActivity {
         stylesButton = findViewById(R.id.stylesButton);
         imgView = findViewById(R.id.profileImage);
 
-        //concatenate first & last name
-        String name = c.getFirstName() + " " + c.getLastName();
 
-        //set data
-        clientName.setText(name);
-        clientDesc.setText(c.getDescription());
-        clientNum.setText(c.getPhoneNumber());
-        imgView.setImageBitmap(imgDecode(c.getEncodedImage()));
+        //fetch customer data from DB and set to object
+        DatabaseReference ref = db.getReference("customers/" + key);
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+
+                    //initialize Client object
+                    Client client = new Client();
+                    
+                    client.setFirstName(dataSnapshot.child("firstName").getValue().toString());
+                    client.setLastName(dataSnapshot.child("lastName").getValue().toString());
+                    client.setDescription(dataSnapshot.child("notes").getValue().toString());
+                    client.setPhoneNumber(dataSnapshot.child("phone").getValue().toString());
+                    client.setEncodedImage(dataSnapshot.child("profile_pic").getValue().toString());
+
+
+                    String name = client.getFirstName() + client.getLastName();
+
+                    clientName.setText(name);
+                    clientDesc.setText(client.getDescription());
+                    clientNum.setText(client.getPhoneNumber());
+                    imgView.setImageBitmap(imgDecode(client.getEncodedImage()));
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
 
         //button action
@@ -51,18 +87,15 @@ public class ProfileActivity extends AppCompatActivity {
             public void onClick(View v){
 
                 //replace with harnoor's styles list page
-                startActivity(new Intent(getApplicationContext(), Client.class));
+                startActivity(new Intent(getApplicationContext(), ImagesActivity.class));
             }
         });
-
-
 
 
     }
 
     //decode string to image
     public Bitmap imgDecode(final String image) {
-        String encodedImage = "";
 
         byte[] decodedBytes = Base64.decode(image, Base64.DEFAULT);
         Bitmap decodedBitmap = BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length);
