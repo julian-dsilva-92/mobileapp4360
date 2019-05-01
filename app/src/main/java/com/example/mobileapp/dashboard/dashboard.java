32ec51d4b894045cde2d21ad174f5f0f19f54af8
juliandsilva <dsilvajulian@yahoo.com>
2019-04-30 23:12:38 -0400

    Added login validation from database and dashboard name feature

--- app/src/main/java/com/example/mobileapp/dashboard/dashboard.java
+++ app/src/main/java/com/example/mobileapp/dashboard/dashboard.java
@@ -2,14 +2,23 @@ package com.example.mobileapp.dashboard;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.mobileapp.R;
 import com.example.mobileapp.calendar.MainCalendarActivity;
 import com.example.mobileapp.camera.CameraActivity;
 import com.example.mobileapp.dB.firebaseDashboard;
+import com.example.mobileapp.loginregistration.LoginActivity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
 
@@ -17,6 +26,7 @@ public class dashboard extends AppCompatActivity {
 
 private RecyclerView mrecyclerview;
 
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,12 +36,37 @@ private RecyclerView mrecyclerview;
         new firebaseDashboard().getdashboarddata(new firebaseDashboard.getallAppts() {
             @Override
             public void Dataisloaded(List<getApptDashboard> appts) {
-                new recylerviewconfig().setconfig(mrecyclerview,dashboard.this, appts);
+                new recylerviewconfig().setconfig(mrecyclerview, dashboard.this, appts);
             }
         });
 
 
+        DatabaseReference ref = database.getReference("hairstylists");
+
+        ref.orderByChild("userEmail").equalTo(LoginActivity.stylistKey).addValueEventListener(new ValueEventListener() {
+
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+
+                for (DataSnapshot datas : dataSnapshot.getChildren()) {
+                    String keys = datas.getKey();
+
+                        getDashboardname(keys);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
+
+
+
     public void ContactList (View v){
         Intent con = new Intent(this, ContactList.class);
         startActivity(con);
@@ -46,4 +81,32 @@ private RecyclerView mrecyclerview;
         Intent cam = new Intent(this, CameraActivity.class);
         startActivity(cam);
     }
+
+
+    public void getDashboardname (String key){
+
+        DatabaseReference ref = database.getReference("hairstylists/" + key);
+        ref.addValueEventListener(new ValueEventListener() {
+
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+        String name  = dataSnapshot.child("fname").getValue(String.class);
+
+                TextView title = findViewById(R.id.textView);
+
+                title.setText("WELCOME " + name);
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+
+        });
+    }
+
 }
--- app/src/main/java/com/example/mobileapp/loginregistration/LoginActivity.java
+++ app/src/main/java/com/example/mobileapp/loginregistration/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.mobileapp.loginregistration;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,10 +11,23 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.mobileapp.R;
+import com.example.mobileapp.calendar.newAppt;
 import com.example.mobileapp.dashboard.dashboard;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class LoginActivity extends AppCompatActivity {
+
+    private String email;
+    private String password;
+    public static String stylistKey;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+
 
     // UI references.
     private EditText mPassword, mEmail;
@@ -30,15 +44,36 @@ public class LoginActivity extends AppCompatActivity {
         btLogin.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                String email = mEmail.getText().toString();
-                String password = mPassword.getText().toString();
-                if (email.equals("abd@gmail.com") && password.equals("test")) { //if password is correct start dashboard
-                    Intent mainIntent = new Intent(LoginActivity.this, dashboard.class);
-                    startActivity(mainIntent);
+                 email = mEmail.getText().toString();
+                 password = mPassword.getText().toString();
+                DatabaseReference ref = database.getReference("hairstylists");
 
-                } else {
-                    Toast.makeText(LoginActivity.this, "Name or Password is incorrect", Toast.LENGTH_LONG).show();
-                }
+                ref.orderByChild("userEmail").equalTo(email).addValueEventListener(new ValueEventListener() {
+
+
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                        if (dataSnapshot.exists())
+                            for (DataSnapshot datas : dataSnapshot.getChildren()) {
+                             String keys = datas.getKey();
+
+                             validateUser(keys, password, email);
+                            }
+                            else{
+                            Toast.makeText(LoginActivity.this, "Name or Password is incorrect", Toast.LENGTH_LONG).show();
+
+                        }
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+
+                });
+
             }
         });
     }
@@ -47,4 +82,36 @@ public class LoginActivity extends AppCompatActivity {
         Intent mainIntent = new Intent(this, NewUser.class);
         startActivity(mainIntent);
     }
+
+    public void validateUser (String key, final String pass, final String user){
+
+        DatabaseReference ref = database.getReference("hairstylists/" + key);
+        ref.addValueEventListener(new ValueEventListener() {
+
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                String passCheck = dataSnapshot.child("pass").getValue(String.class);
+                if (passCheck.equals(pass)){
+                    stylistKey =  user;
+                    Intent mainIntent = new Intent(LoginActivity.this, dashboard.class);
+                    startActivity(mainIntent);
+
+                }
+                else {
+                    Toast.makeText(LoginActivity.this, "Name or Password is incorrect", Toast.LENGTH_LONG).show();
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+
+        });
+        }
+
 }
